d459a0d2dd99300b4ced23d805e279aeb2df3f5d
eriksauerbier <erik.sauerbier@mailbox.org>
2024-02-21 13:27:49 +0100

    .

---
+++ ExchangeDiensteStarten.ps1
@@ -0,0 +1,54 @@
+﻿# Automatic services
+$auto = "MSExchangeADTopology",
+"MSExchangeAntispamUpdate",
+"MSExchangeDagMgmt",
+"MSExchangeDiagnostics",
+"MSExchangeEdgeSync",
+"MSExchangeFrontEndTransport",
+"MSExchangeHM",
+"MSExchangeImap4",
+"MSExchangeIMAP4BE",
+"MSExchangeIS",
+"MSExchangeMailboxAssistants",
+"MSExchangeMailboxReplication",
+"MSExchangeDelivery",
+"MSExchangeSubmission",
+"MSExchangeRepl",
+"MSExchangeRPC",
+"MSExchangeFastSearch",
+"HostControllerService",
+"MSExchangeServiceHost",
+"MSExchangeThrottling",
+"MSExchangeTransport",
+"MSExchangeTransportLogSearch",
+"MSExchangeUM",
+"MSExchangeUMCR",
+"FMS",
+"IISADMIN",
+"RemoteRegistry",
+"SearchExchangeTracing",
+"Winmgmt",
+"W3SVC"
+
+# Manual services
+$man = "MSExchangePop3",
+"MSExchangePOP3BE",
+"wsbexchange",
+"AppIDSvc",
+"pla"
+
+# Enable Services
+foreach ($service in $auto) {
+   Set-Service -Name $service -StartupType Automatic
+   Write-Host "Enabling "$service
+}
+foreach ($service2 in $man) {
+   Set-Service -Name $service2 -StartupType Manual
+   Write-Host "Enabling "$service2
+}
+
+# Start Services
+foreach ($service in $auto) {
+   Start-Service -Name $service
+   Write-Host "Starting "$service 
+}
